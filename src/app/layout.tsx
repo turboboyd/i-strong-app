53b1e7062dad70a70930dd3de8bb2c985f3ba9f3
Denis Daniv <denisdaniv1@gmail.com>
2024-06-08 00:01:31 +0200

    аф

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -66,7 +66,32 @@ const RootLayout: FC<Readonly<IRootLayout>> = ({ home, entry }) => {
   }
 
   scheduleNotification()
-
+  const scheduleNotification2 = async () => {
+    await LocalNotifications.requestPermissions()
+  
+    await LocalNotifications.schedule({
+      notifications: [
+        {
+          title: 'Тестовое уведомление',
+          body: 'Это тестовое уведомление, которое приходит каждую минуту.',
+          id: 1,
+          schedule: { every: 'minute' },
+          actionTypeId: '',
+          extra: null,
+        },
+        {
+          title: 'Пора спать',
+          body: 'Уже 00:10, пора идти спать.',
+          id: 2,
+          schedule: { at: new Date(new Date().setHours(0, 10)) },
+          actionTypeId: '',
+          extra: null,
+        },
+      ],
+    })
+  }
+  
+  scheduleNotification2()
   //return
   return (
     <html lang='uk' className={mainFont.className}>
