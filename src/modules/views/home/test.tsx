7a65d1da0bcd00d1987bc3a050e0fb49f7ec474a
Denis Daniv <denisdaniv1@gmail.com>
2024-06-08 16:29:57 +0200

    ad

--- src/modules/views/home/test.tsx
+++ src/modules/views/home/test.tsx
@@ -2,40 +2,78 @@ import { Keyboard } from '@capacitor/keyboard'
 
 import { useEffect } from 'react'
 
-const useHideKeyboardOnClickOutside = () => {
+const useKeyboard = () => {
   useEffect(() => {
-    const hideKeyboard = async () => {
-      await Keyboard.hide()
-    }
-
-    const handleClickOutside = (event: MouseEvent) => {
-      const target = event.target as HTMLElement
-      if (!target.closest('input')) {
-        hideKeyboard()
-      }
-    }
-
-    // Disable automatic scrolling
-    const disableScroll = async () => {
+    const handleKeyboardWillShow = async (info: any) => {
+      console.log('Keyboard will show', info)
       await Keyboard.setScroll({ isDisabled: true })
     }
 
-    disableScroll()
+    const handleKeyboardDidShow = (info: any) => {
+      console.log('Keyboard did show', info)
+    }
 
+    const handleKeyboardWillHide = async () => {
+      console.log('Keyboard will hide')
+      await Keyboard.setScroll({ isDisabled: false })
+    }
+
+    const handleKeyboardDidHide = () => {
+      console.log('Keyboard did hide')
+    }
+
+    const handleScroll = () => {
+      Keyboard.hide()
+    }
+
+    const handleClickOutside = (event: Event) => {
+      const target = event.target as HTMLElement
+      if (target.tagName !== 'INPUT' && target.tagName !== 'TEXTAREA') {
+        Keyboard.hide()
+      }
+    }
+
+    const handleFocus = async () => {
+      await Keyboard.setScroll({ isDisabled: true })
+      await Keyboard.setAccessoryBarVisible({ isVisible: true })
+    }
+
+    const handleBlur = async () => {
+      await Keyboard.setScroll({ isDisabled: false })
+      await Keyboard.setAccessoryBarVisible({ isVisible: false })
+    }
+
+    // Подписка на события показа и скрытия клавиатуры
+    Keyboard.addListener('keyboardWillShow', handleKeyboardWillShow)
+    Keyboard.addListener('keyboardDidShow', handleKeyboardDidShow)
+    Keyboard.addListener('keyboardWillHide', handleKeyboardWillHide)
+    Keyboard.addListener('keyboardDidHide', handleKeyboardDidHide)
+
+    // Подписка на события прокрутки и клика вне инпута
+    window.addEventListener('scroll', handleScroll)
     document.addEventListener('click', handleClickOutside)
+    document.addEventListener('focusin', handleFocus)
+    document.addEventListener('focusout', handleBlur)
 
     return () => {
+      // Отписка от событий при размонтировании компонента
+      Keyboard.removeAllListeners()
+      window.removeEventListener('scroll', handleScroll)
       document.removeEventListener('click', handleClickOutside)
+      document.removeEventListener('focusin', handleFocus)
+      document.removeEventListener('focusout', handleBlur)
     }
   }, [])
 }
 
-const MyComponent = () => {
-  useHideKeyboardOnClickOutside()
-
+const MyComponent: React.FC = () => {
+  useKeyboard()
   return (
     <div>
-      <input type='text' />
+      <h1>Контроль прокрутки клавиатуры</h1>
+      <input type='text' placeholder='Введите текст' />
+      <input type='password' placeholder='Введите пароль' />
+      <textarea placeholder='Введите сообщение'></textarea>
     </div>
   )
 }
